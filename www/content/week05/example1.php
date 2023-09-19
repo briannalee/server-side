75a1c7cebf2bac4d6a2539824e4fd1745833c070
Brianna Ragsdale <mail@brianna.cc>
2023-09-19 13:48:53 -0500

    Week 5 - Part 1

---
+++ www/content/week05/example1.php
@@ -0,0 +1,8 @@
+
+	<?php include("/Applications/www/server-side/www/content/week05/header2.php"); ?>
+	<!-- MAIN FOES HERE -->
+	<div id="mainBox">
+		<h1>Using include and require functions in PHP</h1>
+		<h2>Week 5 Example 1</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at purus non ex laoreet molestie. Aliquam ac hendrerit urna. Maecenas ut metus ut magna tempor vulputate. In mattis nisl nisl, sed bibendum lacus consectetur ac. Vestibulum hendrerit nunc non diam venenatis, quis lobortis risus commodo. Aliquam dignissim, tortor non congue sollicitudin, sem diam commodo odio, nec facilisis odio metus sed enim. Cras in tincidunt nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc ac neque scelerisque, varius eros sed, tincidunt neque. In commodo malesuada tellus, at euismod urna elementum iaculis. Quisque iaculis ac quam sit amet aliquam. Aenean imperdiet augue id mauris sodales, eget pellentesque orci molestie. Fusce eget lectus vel ante elementum finibus. Duis nec leo vel metus sollicitudin tempor. Proin eu porttitor turpis.</p>
+	</div>
---
+++ www/content/week05/example5a.php
@@ -0,0 +1,7 @@
+<?php
+		// Due to my setup, full path is required to include... will fix
+		include('/Applications/www/server-side/www/content/week05/header2.php');
+
+		include('/Applications/www/server-side/www/content/week05/sectionA.php');
+
+		//My footer is included via Smarty template, not included here
